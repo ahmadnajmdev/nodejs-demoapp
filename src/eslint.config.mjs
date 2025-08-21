85d5607ade667919d3b9c2e07a2303f218e661c5
Ben Coleman <benc.uk@gmail.com>
2025-08-21 14:51:19 +0100

    Update .env.sample and README for clarity; add Redis session note and fix typo in test documentation (#42)

--- src/eslint.config.mjs
+++ src/eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from 'globals'
 import js from '@eslint/js'
 import eslintConfigPrettier from 'eslint-config-prettier'
 
+
 export default [
   js.configs.recommended,
   eslintConfigPrettier,
@@ -31,4 +32,4 @@ export default [
       'prefer-const': 'error',
     },
   },
-]
+];
